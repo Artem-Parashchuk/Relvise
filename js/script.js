e5316594dbec435493077a49ae718842fd6bbd5b
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-02-26 11:21:41 +0200

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
-const btnMenu = document.querySelector('.menu-open__btn')// Кнопка меню
-const btnCloseMenu = document.querySelector('.menu__close-btn')// Кнопка закривання меню
-const menuList = document.querySelector('.header__nav-list')  // Мобільне меню
-const menuClose = document.querySelector('.menu-close')//  div для створення фону при відкритому меню 
+const btnMenu = document.querySelector('.mobile__menu-btn')// Кнопка меню
+// const btnCloseMenu = document.querySelector('.menu__close-btn')// Кнопка закривання меню
+// const menuList = document.querySelector('.header__nav-list')  // Мобільне меню
+// const menuClose = document.querySelector('.menu-close')//  div для створення фону при відкритому меню 
+
+const mobileMenuList = document.querySelector('.mobile__menu-list')
+
+
+
 
 
 btnMenu.addEventListener('click', (e) => {
-    menuList.classList.add('nav-list-open')
-    menuClose.classList.add('is-open')
+    btnMenu.classList.toggle('active')
+    mobileMenuList.classList.toggle('active')
 })
 
-btnCloseMenu.addEventListener('click', (e) => {
-    menuList.classList.remove('nav-list-open')
-    menuClose.classList.remove('is-open')
-})
+// btnCloseMenu.addEventListener('click', (e) => {
+//     menuList.classList.remove('nav-list-open')
+//     menuClose.classList.remove('is-open')
+// })
 
-document.addEventListener('click', (e) => {
-    if(e.target === menuClose) {
-        menuList.classList.remove('nav-list-open')
-        menuClose.classList.remove('is-open')
-    }
-})
+// document.addEventListener('click', (e) => {
+//     if(e.target === menuClose) {
+//         menuList.classList.remove('nav-list-open')
+//         menuClose.classList.remove('is-open')
+//     }
+// })
+
